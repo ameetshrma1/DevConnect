2aeb04215944ae5596a2f4cb52d127dad433e9a7
prasharma0 <prashant.sharma.ps725@gmail.com>
2021-07-30 14:19:50 +0545

    USER_ADDED, AUTH_ERROR

--- client/src/actions/auth.tsx
+++ client/src/actions/auth.tsx
@@ -4,7 +4,30 @@ import { any } from "prop-types";
 import { async } from "q";
 import { setAlert } from "./alert";
 
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from "./types";
+import setAuthToken from "../utils/setAuthToken";
+//Load USer
+export const loadUser = () => async (dispatch: any) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get("/api/auth");
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
 
 interface IUser {
   email: string;
