1fc0c2098d4b551e51a58b444bef0ce01bdc5b91
Ameet Shrma <ameetshrma@outlook.com>
2021-08-16 13:29:32 +0545

    some fixes

--- server.js
+++ server.js
@@ -2,25 +2,33 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
 
 //Connect Database
 
 connectDB();
-
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("API running");
-});
-
 //Define Routes
 
-app.use("/api/users", require("./routes/api/users")); //RESTFULL
-app.use("/api/auth", require("./routes/api/auth")); //RESTFULL
-app.use("/api/profile", require("./routes/api/profile")); //RESTFULL
-app.use("/api/posts", require("./routes/api/posts")); //RESTFULL
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
